0f3b7fc9cebe8c89dd1a71ef584b50fc403d0494
ElizavetaBodrova <bear147!>
2021-03-29 09:55:32 +0300

    V-F

--- src/VagnerPhisher/MainVagnerPhisher.java
+++ src/VagnerPhisher/MainVagnerPhisher.java
@@ -9,7 +9,7 @@ public class MainVagnerPhisher {
         String str1 = scanner.nextLine();
         System.out.print("Enter str2: ");
         String str2 = scanner.nextLine();
-        System.out.println(VagnerPhisher.levenstain(str1, str2));
+        VagnerPhisher.levenstain(str1, str2);
         System.out.println("Compares count: " + VagnerPhisher.counter);
     }
 }
--- src/VagnerPhisher/VagnerPhisher.java
+++ src/VagnerPhisher/VagnerPhisher.java
@@ -1,34 +1,82 @@
 package VagnerPhisher;
 
-public class VagnerPhisher {
-    public static int counter=0;
 
+public class VagnerPhisher {
+    public static int counter;
+
+    public static void print(int[][] array){
+        System.out.println("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println("]");
+        }
+        System.out.println("]");
+    }
 
     public static int levenstain(String str1, String str2) {
-        counter=0;
-        int[] Di_1 = new int[str2.length() + 1];
-        int[] Di = new int[str2.length() + 1];
-
-        for (int j = 0; j <= str2.length(); j++) {
-            Di[j] = j; // (i == 0)
+        counter = 0;
+        if (str1.length() == 0) {
+            return str2.length();
+        }
+        if (str2.length() == 0) {
+            return str1.length();
         }
 
-        for (int i = 1; i <= str1.length(); i++) {
-            System.arraycopy(Di, 0, Di_1, 0, Di_1.length);
-
-            Di[0] = i; // (j == 0)
-            for (int j = 1; j <= str2.length(); j++) {
-                int cost = (str1.charAt(i - 1) != str2.charAt(j - 1)) ? 1 : 0;
-                counter++;
-                Di[j] = min(
-                        Di_1[j] + 1,
-                        Di[j - 1] + 1,
-                        Di_1[j - 1] + cost
-                );
+        int priceDelete = 1;
+        int pricePaste = 1;
+        int[][] D = new int[str1.length() + 1][str2.length() + 1];
+        for (int i = 1; i < str1.length() + 1; i++) {
+            D[i][0] = D[i - 1][0] + 1;
+            for (int j = 1; j < str2.length() + 1; j++) {
+                D[0][j] = D[0][j - 1] + 1;
             }
         }
+        System.out.print("D=");
+        print(D);
+        int check;
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                counter++;
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    check = 0;
+                }
+                else {
+                    check = 2;
+                }
+                D[i][j]=min(D[i-1][j]+priceDelete, D[i][j-1]+pricePaste, D[i-1][j-1]+check);
 
-        return Di[Di.length - 1];
+            }
+
+        }
+        System.out.print("D=");
+        print(D);
+        System.out.println("Расстояние между строками "+D[str1.length()][str2.length()]);
+
+        int i = str1.length();
+        int j = str2.length();
+        System.out.println("След:");
+        System.out.println("["+i+","+j+"]");
+        while(i>1 && j>1){
+            if(D[i-1][j-1]==D[i-2][j-1]+priceDelete){
+                i--;
+            }
+            else{
+                if(D[i-1][j-1]==D[i-1][j-2]+pricePaste){
+                    j--;
+                }
+                else{
+                    if(str1.charAt(i-2)==str2.charAt(j-2) && (i-1!=0 && j-1!=0)){
+                        System.out.println("["+(i-1)+","+(j-1)+"]");
+                        i--;
+                        j--;
+                    }
+                }
+            }
+        }
+        return D[str1.length()][str2.length()];
     }
 
     private static int min(int n1, int n2, int n3) {
